f23996f74ea0f237e4561e22a73a66c64d9a7b1f
michaels <michael.shevelin@aradtec.com>
2025-03-12 10:25:05 +0200

    Initial commit

---
+++ TestProject/IFlowDataParser.cs
@@ -0,0 +1,29 @@
+﻿using Assignment;
+
+namespace TestProject
+{
+    /// <summary>
+    /// Interface for a flow data parser that handles BLE device data using a custom record type
+    /// </summary>
+    /// <typeparam name="TRecord">The flow record type to be created by the implementer</typeparam>
+    public interface IFlowDataParser<TRecord>
+    {
+        /// <summary>
+        /// Handles raw data received from the BLE device
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">Event arguments containing the raw data</param>
+        void HandleRawData(object? sender, RawDataEventArgs e);
+
+        /// <summary>
+        /// Clears all parsed records
+        /// </summary>
+        void ClearRecords();
+
+        /// <summary>
+        /// Gets all parsed records
+        /// </summary>
+        /// <returns>List of flow records</returns>
+        List<TRecord> GetRecords();
+    }
+}
